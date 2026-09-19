d5378e9efcc19b88f9c4d0941132388b58313019
Oman Vasquez <omanvasquez@gmail.com>
2026-09-18 21:02:25 -0400

    feat(android): add Android TWA wrapper project and Digital Asset Links

---
+++ android-twa/app/src/main/java/com/comerciopro/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.comerciopro.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
